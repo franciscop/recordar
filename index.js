8d2dc140099ad9905168a48edf197d9608645629
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-02-01 22:43:02 +0100

    First release

---
+++ index.js
@@ -0,0 +1,94 @@
+// Recordar - Gives a 0.0-1.0 range, the higher the more you remember it
+const bound = num => Math.min(Math.max(0, num), 1);
+
+const defaults = {
+
+  // The half-life of each answer. Set higher for easier sets or fast learners
+  halflife: 3600,
+
+  // The minimum that each try will bias the solution (min, 1)
+  minimum: 0.5
+};
+
+// The main function. Accepts options and then a list of the actual tries
+// recordar({})([{}, {}, {}]).then(score => { ... });
+const recordar = (tries = [], options = {}) => new Promise((resolve, reject) => {
+  if (!(tries instanceof Array)) {
+    throw new Error('The parameter should be an array of tries');
+  }
+  if(tries.length === 0) {
+    return resolve(0.5);
+  }
+
+  recordar.options = Object.assign({}, recordar.options, options);
+
+  Object.keys(recordar.factors).reduce((all, key) => {
+    return all.then(glo => recordar.factors[key](tries).then(local => {
+      return glo * local * 2;
+    }));
+  }, Promise.resolve(0.5)).then(sol => resolve(bound(sol)));
+});
+
+
+recordar.options = defaults;
+
+
+// Every one of the factors range from 0 to 1, averagin around 0.5
+// Since they have to be multiplied together, each is duplicated before
+recordar.factors = {};
+
+// Time factor; the longer the time, the more you don't know
+recordar.factors.forget = tries => new Promise((resolve, reject) => {
+  if (!tries.length) return resolve(0.5);
+
+  let last = tries.map(w => w.time).sort().shift();
+  if (!last) return resolve(0.5);
+  // Wolfram Alpha: 1 - ln(x) / (2 * ln(3600)) from 0 to 10000
+  // 0.5 ~= 1 - ln(x) / 2 * ln(const)
+  let selffactor = 2 * Math.log(recordar.options.halflife);
+  let solution = 1 - Math.log((new Date() - last) / 1000) / selffactor;
+  resolve(bound(solution));
+});
+
+// Accuracy factor; the more errors you make, the less that you know
+// Old errors/rights account less than newer ones
+recordar.factors.accuracy = tries => new Promise((resolve, reject) => {
+  if (!tries.length) return resolve(1);
+
+  // Forget 50% in e ^ (-A * t)
+  let coeffs = {
+    hour: 0.0002,  // 1h
+    day: 0.000008  // 24h
+  };
+
+  let size = (all, one) => {
+    let timediff = (new Date() - one.time) / 1000;
+    let remember = Math.pow(Math.E, (-coeffs.day * timediff));
+    // Force each try to always influence even if it's just a bit
+    remember = Math.max(remember, recordar.options.minimum);
+    return all + remember;
+  };
+
+  var good = tries.filter(n => n.type === 'good').reduce(size, 1);
+  var bad = tries.filter(n => n.type === 'bad').reduce(size, 1);
+  var total = 0.5 + 0.5 * (good - bad) / (good + bad);
+  resolve(bound(total));
+});
+
+// Position of the index vs total size
+// TODO: dynamic range depending on the dataset size
+// TODO: push this up
+// recordar.factors.index = function(word, i, all) {
+//   return 1.2 - 0.4 * (word.index / all.length);
+// };
+
+// Make it slightly random
+// recordar.factors.random = function () {
+//   return 0.2 * Math.random() + 0.9;
+// };
+
+
+
+if (typeof module !== 'undefined') {
+  module.exports = recordar;
+}
---
+++ recordar.test.js
@@ -0,0 +1,200 @@
+// let recordar = require('./index.js');
+const recordar = require('./index');
+
+const timeago = time => {
+  if (typeof time === 'number') time = { seconds: time };
+  const diff = (time.seconds || 0) +
+    (time.minutes || 0) * 60 +
+    (time.hours || 0) * 3600 +
+    (time.days || 0) * 24 * 3600;
+  let d = new Date();
+  d.setTime((d.getTime() / 1000 - diff) * 1000);
+  return d;
+};
+
+const good = ago => ({ type: 'good', time: timeago(ago) });
+const bad = ago => ({ type: 'bad', time: timeago(ago) });
+
+describe('constructor', () => {
+  it('can build with nothing', () => {
+    return recordar().then(score => expect(score).toBe(0.5));
+  });
+
+  it('can build with empty array', () => {
+    return recordar([]).then(score => expect(score).toBe(0.5));
+  });
+
+  it('does not accept numbers', () => {
+    return recordar(1).catch(err => expect(err).toBeDefined());
+  });
+
+  it('does not accept strings', () => {
+    return recordar('a').catch(err => expect(err).toBeDefined());
+  });
+
+  it('does not accept objects', () => {
+    return recordar({}).catch(err => expect(err).toBeDefined());
+  });
+});
+
+
+describe('factors:forget', () => {
+  it('is 0.5 for half-life', () => {
+    return recordar.factors.forget([good(3600)]).then(sol => expect(sol).toBe(0.5));
+  });
+});
+
+
+describe('bound checking', () => {
+  it('never goes below 0', () => {
+    return recordar([good(1000000000)]).then(min => expect(min).toBe(0));
+  });
+});
+
+
+describe('comparing instances', () => {
+  it('older are forgotten more than recent', () => {
+    let solutions = [recordar([good(100)]), recordar([good(10)])];
+    return Promise.all(solutions).then(([older, recent]) => {
+      expect(older).toBeLessThan(recent);
+    });
+  });
+
+  it('handles failures vs correct', () => {
+    let solutions = [recordar([good(10)]), recordar([bad(10)])];
+    return Promise.all(solutions).then(([ok, no]) => {
+      expect(no).toBeLessThan(ok);
+    });
+  });
+
+  it('handles multiple failures', () => {
+    let a = recordar([bad(10)]);
+    let b = recordar([bad(10), bad(10)]);
+    let c = recordar([bad(10), bad(10), bad(10)]);
+    let d = recordar([bad(10), bad(10), bad(10), bad(10)]);
+    let e = recordar([bad(10), bad(10), bad(10), bad(10), bad(10)]);
+    let f = recordar([bad(10), bad(10), bad(10), bad(10), bad(10), bad(10)]);
+
+    let solutions = [a, b, c, d, e, f];
+    return Promise.all(solutions).then(([a, b, c, d, e, f]) => {
+      expect(a).toBeGreaterThan(b);
+      expect(b).toBeGreaterThan(c);
+      expect(c).toBeGreaterThan(d);
+      expect(d).toBeGreaterThan(e);
+      expect(e).toBeGreaterThan(f);
+    });
+  });
+
+
+  // it('returns by failures', () => {
+  //   let set = [
+  //     point('set:0:a', [good(10)]),
+  //     point('set:0:b', [good(10)]),
+  //     point('set:0:c', [good(10)]),
+  //     point('set:0:d', [good(10)]),
+  //     point('set:0:e', [bad(10)])
+  //   ];
+  //   expect(recordar(set).word).toBe('e');
+  // });
+  //
+  // it('returns by time', () => {
+  //   let set = [
+  //     point('set:0:a', [good(10)]),
+  //     point('set:0:b', [good(10)]),
+  //     point('set:0:c', [good(10)]),
+  //     point('set:0:d', [good(10)]),
+  //     point('set:0:e', [good(10000)])
+  //   ];
+  //   expect(recordar(set).word).toBe('e');
+  // });
+  //
+  // it('half-life can be adjusted', () => {
+  //   let data = point('set:0:a', [good(10)]);
+  //   recordar.options.halflife = 3600;
+  //   let chance = recordar.factors.forget(point('', [ good({ hours: 1 }) ]));
+  //   expect(Math.round(chance * 100)).toBe(100);
+  //
+  //   recordar.options.halflife = 24 * 3600;
+  //   chance = recordar.factors.forget(point('', [ good(24 * 3600) ]));
+  //   expect(Math.round(chance * 100)).toBe(100);
+  //
+  //   recordar.options.halflife = 3600;
+  // });
+  //
+  // it('recent events are more relevant', () => {
+  //   let set = [
+  //     recordar.factors.accuracy(point('set:0:a', [bad(1)])),
+  //     recordar.factors.accuracy(point('set:0:a', [bad({ hours: 1 })])),
+  //     recordar.factors.accuracy(point('set:0:a', [bad({ hours: 12 })])),
+  //     recordar.factors.accuracy(point('set:0:a', [bad({ days: 1 })]))
+  //   ];
+  //
+  //   // Older events are closer to 1 (less relevant)
+  //   for (let i = 1; i < set.length; i++) {
+  //     closerToOne(set[i], set[i-1]);
+  //   }
+  //
+  //   set = [
+  //     recordar.factors.accuracy(point('set:0:a', [good(1)])),
+  //     recordar.factors.accuracy(point('set:0:a', [good({ hours: 1 })])),
+  //     recordar.factors.accuracy(point('set:0:a', [good({ hours: 12 })])),
+  //     recordar.factors.accuracy(point('set:0:a', [good({ days: 1 })]))
+  //   ];
+  //
+  //   for (let i = 1; i < set.length; i++) {
+  //     closerToOne(set[i], set[i-1]);
+  //   }
+  // });
+  //
+  // it('is more important arecent mistake than old success', () => {
+  //   let datapoint = point('set:0:a', [bad(10), good({ days: 1 })]);
+  //   let factor = recordar.factors.accuracy(datapoint);
+  //   expect(factor).toBeLessThan(1.5);
+  //   expect(1).toBeLessThan(factor);
+  // });
+  //
+  // it('is more important a recent success than an old mistake', () => {
+  //   let datapoint = point('set:0:a', [good(10), bad({ days: 1 })]);
+  //   let factor = recordar.factors.accuracy(datapoint);
+  //   expect(factor).toBeLessThan(1);
+  //   expect(0.5).toBeLessThan(factor);
+  // });
+
+
+
+
+
+  // describe('some persona tests', () => {
+  //
+  //   it('recent good nullify really old bad', () => {
+  //     let chance = recordar([
+  //       point('set:0:a', [good(10), bad({ days: 1 })])
+  //     ]).chance;
+  //
+  //     // console.log(chance);
+  //     expect(chance < 0.2).toBe(true);
+  //   });
+  //
+  //   it('old enough and super old are the same', () => {
+  //     let chance = recordar([
+  //       point('set:0:a', [good({ days: 20 }), bad({ days: 20 })])
+  //     ]).chance;
+  //
+  //     let chanceb = recordar([
+  //       point('set:0:a', [good({ days: 100 }), bad({ days: 100 })])
+  //     ]).chance;
+  //
+  //     // console.log(Math.round(chance * 10), Math.round(chanceb * 10));
+  //     expect(Math.round(chance * 10)).toBe(Math.round(chanceb * 10));
+  //   });
+  //
+  //   it('many good nullify bad', () => {
+  //     let chance = recordar([
+  //       point('set:0:a', [good(10), good(10), good(10), good(10), bad(10)])
+  //     ]).chance;
+  //
+  //     // console.log(chance);
+  //     expect(chance < 0.5).toBe(true);
+  //   });
+  // });
+});
